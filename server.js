d8fb53409dfc2c4aa9d693585bc2fed13f41766d
mukeshkumawath <mukesh.s.kumawath@gmail.com>
2017-02-10 20:22:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/articleone',function(req,res){
+    res.send("the ulr which is running is article one");
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
